67301c7462a1a2aa25a4b5c70b063a70240d8232
haris123 <har1596@hotmail.com>
2014-11-17 14:49:45 +0100

    dd

--- Servis/Desktop/ViewModel/MainWindowViewModel.cs
+++ Servis/Desktop/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Desktop.ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel:INotifyPropertyChanged, IDataErrorInfo
     {
+        #region Fields
+        private string _imeKorisnika; 
+        #endregion
+
+        #region Properties
+        public string ImeKorisnika
+        {
+            get { return _imeKorisnika; }
+            set { _imeKorisnika = value; OnPropertyChanged("ImeKorisnika") }
+        } 
+        #endregion
+
+        #region INoftiyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        } 
+        #endregion
+
     }
 }
