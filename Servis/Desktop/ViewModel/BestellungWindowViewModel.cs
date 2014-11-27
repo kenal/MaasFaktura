e939a56cd44b8cda0ad69b0ab65444f217f6fa2c
kenal <kenan.alijanovic@gmail.com>
2014-11-27 08:22:43 +0100

    as

---
+++ Servis/Desktop/ViewModel/BestellungWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Desktop.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Desktop.ViewModel
+{
+    public class BestellungWindowViewModel : ICommand
+    {
+        #region ICommand
+        MassServisClient client = new MassServisClient();
+        ObservableCollection<tbl_dobavljac> ListaDobavljaca;
+        #endregion
+    }
+}
