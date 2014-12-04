4ef9ecd0fc8ac7992afbd2a4cd413e8fcc1b12f0
kenal <kenan.alijanovic@gmail.com>
2014-12-04 16:09:22 +0100

    komit

--- Servis/Desktop/ViewModel/KalendarRadnikWindowViewModel.cs
+++ Servis/Desktop/ViewModel/KalendarRadnikWindowViewModel.cs
@@ -25,7 +25,7 @@ namespace Desktop.ViewModel
         private MassServisClient client = new MassServisClient();
         private ObservableCollection<tbl_korisnik> _listaKorisnika;
         private tbl_korisnik _selektovaniKorisnik;
-        private List<int> _ListaSati = new List<int>() { 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };        
+        private List<int> _ListaSati = new List<int>() { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 };        
         private List<int> _ListaMinuta = new List<int>() { 0, 10, 20, 30, 40, 50 };
         int _sat;        
         int _minuta;       
@@ -425,6 +425,7 @@ namespace Desktop.ViewModel
             
             KalendarRadnikDodajWindow krdw = new KalendarRadnikDodajWindow(this);
             krdw.Show();
+            
 
         }
         #endregion
