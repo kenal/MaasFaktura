4d3a650977482ebbfbed40a8eacff27dbd00724f
kenal <kenan.alijanovic@gmail.com>
2014-12-29 16:01:57 +0100

    20141229-9

---
+++ Servis/Desktop/HelperClass/BackgroundConverterPoruke.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Desktop.HelperClass
+{
+    public class BackgroundConverterPoruke: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            ImageSource result = null;
+            if((bool)value == false)
+            {
+                result = new BitmapImage(new Uri("ikone/star-empty.png", UriKind.Relative));
+            }
+            else
+            {
+                result = new BitmapImage(new Uri("ikone/star.png", UriKind.Relative));
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
